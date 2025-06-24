57408de75473585a1d1502fcac7d3d8d93a3bf98
Victor Oliveira <133714871+Oliveira-Jovs@users.noreply.github.com>
2025-06-24 14:22:29 -0300

    Update MainActivity.kt

--- FEJ/app/src/main/java/com/aula/mobile_fej_todo/MainActivity.kt
+++ FEJ/app/src/main/java/com/aula/mobile_fej_todo/MainActivity.kt
@@ -6,23 +6,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-<<<<<<< HEAD
 import androidx.appcompat.widget.Toolbar
-=======
->>>>>>> 57c416827f0a9ed2b5c393fd0a555ff7d7a66399
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
         // ⚠️ IMPORTANTE: configure a Toolbar
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-=======
->>>>>>> 57c416827f0a9ed2b5c393fd0a555ff7d7a66399
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
